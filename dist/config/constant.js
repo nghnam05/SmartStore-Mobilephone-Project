3c66606e5016af56e4c18378b5359607f93bfdb9
Hoai Nam <nhoaiinamm05@gmail.com>
2025-06-29 12:31:07 +0700

    update path

--- dist/config/constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ACCOUNT_TYPE = void 0;
-exports.ACCOUNT_TYPE = {
-    SYSTEM: "SYSTEM",
-    GOOGLE: "",
-    GITHUB: "",
-};
-//# sourceMappingURL=constant.js.map
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,20 +1,25 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-
 dotenv.config();
 const getConnect = async () => {
-  const connection = await mysql.createConnection(
-    process.env.DATABASE_URL as string
-  );
-  return connection;
-  // const connection = await mysql.createConnection({
-  //   port: 3306,
-  //   host: "localhost",
-  //   user: "root",
-  //   password: "123456",
-  //   database: "userdashboard",
-  // });
-  // return connection;
-};
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT || 3306), // ép kiểu về number
+    ssl:
+      process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined,
+  });
 
+  return connection;
+};
 export default getConnect;
+
+// const connection = await mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "userdashboard",
+// });
