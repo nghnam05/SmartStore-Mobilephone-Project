b884d9033da8783b34e4a4c92d75a8b3f45d3ba3
Hoai Nam <nhoaiinamm05@gmail.com>
2025-07-04 15:27:33 +0700

    Update database config and add backup.sql

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,4 +1,5 @@
 "use strict";
+// import mysql from "mysql2/promise";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,23 +13,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const getConnect = async () => {
+//   const connection = await mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//     database: "userdashboard",
+//   });
+//   return connection;
+// };
+// export default getConnect;
+// // const connection = await mysql.createConnection({
+// //   host: process.env.DB_HOST,
+// //   port: Number(process.env.DB_PORT),
+// //   user: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME,
+// // });
 const promise_1 = __importDefault(require("mysql2/promise"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const getConnect = () => __awaiter(void 0, void 0, void 0, function* () {
     const connection = yield promise_1.default.createConnection({
-        port: 3306,
-        host: "localhost",
-        user: "root",
-        password: "123456",
-        database: "userdashboard",
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
+    console.log("✅ DB connected!");
     return connection;
 });
 exports.default = getConnect;
-// const connection = await mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
 //# sourceMappingURL=database.js.map
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,22 +1,40 @@
+// import mysql from "mysql2/promise";
+
+// const getConnect = async () => {
+//   const connection = await mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//     database: "userdashboard",
+//   });
+//   return connection;
+// };
+
+// export default getConnect;
+
+// // const connection = await mysql.createConnection({
+// //   host: process.env.DB_HOST,
+// //   port: Number(process.env.DB_PORT),
+// //   user: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME,
+// // });
+
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 const getConnect = async () => {
   const connection = await mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "userdashboard",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
+  console.log("✅ DB connected!");
   return connection;
 };
 
 export default getConnect;
-
-// const connection = await mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
