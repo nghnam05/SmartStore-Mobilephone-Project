68551b4dc091a957b653fa145fa004d658691b1f
Hoai Nam <nhoaiinamm05@gmail.com>
2025-07-04 15:53:44 +0700

    fix: switch mysql connection to use connection pool

--- dist/config/database.js
+++ dist/config/database.js
@@ -34,14 +34,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const pool = promise_1.default.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // có thể chỉnh tùy theo app
+    queueLimit: 0,
+});
 const getConnect = () => __awaiter(void 0, void 0, void 0, function* () {
-    const connection = yield promise_1.default.createConnection({
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    });
+    const connection = yield pool.getConnection();
     console.log("✅ DB connected!");
     return connection;
 });
